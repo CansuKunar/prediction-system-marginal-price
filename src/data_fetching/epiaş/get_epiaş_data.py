19373bce36eaff35c6d2195b9996b50e678c90c7
Cansu Künar <cansukunar@gmail.com>
2024-10-25 17:40:54 +0300

    Update get_epiaş_data.py

--- src/data_fetching/epiaş/get_epiaş_data.py
+++ src/data_fetching/epiaş/get_epiaş_data.py
@@ -30,33 +30,48 @@ data_columns = {
 }
 
 
-dataframes = {}
-
 def fetch_epias_data(data_type, start_date, end_date):
     """
     Retrieves data from Epiaş for the given data_type.
     """
     return eptr.call(data_type, start_date=start_date, end_date=end_date)
 
-for key, columns in data_columns.items():
-    df = fetch_epias_data(key, start_date, end_date)
-    df = pd.DataFrame(df)  # Convert incoming data to DataFrame
+def get_merged_epias_data(start_date, end_date):
+    """
+    Fetches and merges EPIAŞ data for the specified date range.
+    Returns a DataFrame with the merged data.
+    """
 
-    # Convert 'date' and 'hour' columns to ensure consistency
-    df['date'] = pd.to_datetime(df['date']).dt.date  # Only retain date part
-    df['hour'] = pd.to_datetime(df['hour']).dt.time  # Extract only the time part
+    dataframes = {}
 
-    df = df[columns]  # Select relevant columns
-    dataframes[key] = df  # Save DataFrame to dictionary
+    for key, columns in data_columns.items():
+        df = fetch_epias_data(key, start_date, end_date)
+        df = pd.DataFrame(df)  # Convert incoming data to DataFrame
 
-# Consolidate data based on columns 'date' and 'hour'
-merged_df = dataframes["mcp"]
-for key, df in dataframes.items():
-    if key != "mcp":
-        merged_df = pd.merge(merged_df, df, on=["date", "hour"], how="outer")
+        # Convert 'date' and 'hour' columns to ensure consistency
+        df['date'] = pd.to_datetime(df['date']).dt.date  # Only retain date part
+        df['hour'] = pd.to_datetime(df['hour']).dt.time  # Extract only the time part
+
+        df = df[columns]  # Select relevant columns
+        dataframes[key] = df  # Save DataFrame to dictionary
 
 
-# print(merged_df.head())
+    # Consolidate data based on columns 'date' and 'hour'
+    merged_df = dataframes["mcp"]
+    for key, df in dataframes.items():
+        if key != "mcp":
+            merged_df = pd.merge(merged_df, df, on=["date", "hour"], how="outer")
 
-path = os.getenv('project_path')
-merged_df.to_csv(path+"/data/raw/merged_epias_data.csv", index=False)
+    return merged_df
+
+def main():
+    # Fetch and merge EPIAŞ data
+    merged_epias_data = get_merged_epias_data(start_date, end_date)
+
+    # Save the merged data to a CSV file
+    path = os.getenv('project_path')
+    merged_epias_data.to_csv(path + "/data/raw/merged_epias_data.csv", index=False)
+    print("Merged EPIAŞ data saved to CSV.")
+
+if __name__ == "__main__":
+    main()
